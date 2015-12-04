9f5e199554cc7c02ddb5a59fd6ddbcecfcd23810
William Gibbins <wgibbins@Williams-MacBook-Pro.local>
2015-12-04 09:32:37 -0500

    Setting up a new repo for unit testing kata

---
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package stringCalculator;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+	@Test
+	public void myFirstTest_IsAFailure() throws Exception {
+		fail();
+	}
+}
